0c418b3db8f59a36683ab4c30f8ae211bcc3cd92
dicacristian <cristian.dica28@gmail.com>
2023-05-02 16:58:36 +0200

    Tema ordonarea numerelor in mod descrescator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.interactions.Actions;
 
 import java.util.List;
 
+//Am incercat sa o rezolv, dar nu am reusit.
 public class Main {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
