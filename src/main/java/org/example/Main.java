50f977af8cb9d74e430fffbe15b0ca03aa1e7537
dicacristian <cristian.dica28@gmail.com>
2023-05-02 20:05:49 +0200

    Tema ordonarea numerelor in mod descrescator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,21 +8,27 @@ import org.openqa.selenium.interactions.Actions;
 
 import java.util.List;
 
-//Am incercat sa o rezolv, dar nu am reusit.
+/**
+ * Tema de weekend: https://demoqa.com/sortable sa se sorteze in ordine descrescatoare elementele din pagina folosind selenium / actions / drag and drop.
+ */
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         ChromeDriver driver = new ChromeDriver();
         driver.get("https://demoqa.com/sortable");
-        String xPathDescedentChilds = "//div[@class ='vertical-list-container mt-4']";
+        String xPathDescedentChilds = "//*[@id='demo-tabpane-list']//*[contains(@class,'list-group-item')]";
         List<WebElement> descedentChilds = driver.findElements(By.xpath(xPathDescedentChilds));
         Actions actions = new Actions(driver);
-        for(int i = descedentChilds.size() - 1 ; i>=0;i--){
-            actions.dragAndDrop(descedentChilds.get(i+1),descedentChilds.get(i)).perform();
-            driver.quit();
+        int i = descedentChilds.size() - 1;
+        Thread.sleep(3000);
 
-
-    }
+        for (int j = 0; j <= i; j++) {
+            actions.dragAndDrop(descedentChilds.get(i), descedentChilds.get(j)).perform();
+            Thread.sleep(1000);
+            descedentChilds = driver.findElements(By.xpath(xPathDescedentChilds));
+        }
+        driver.quit();
     }
 }
 
