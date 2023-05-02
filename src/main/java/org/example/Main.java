9a5156e9e400c193d8f7796ca31fd9249838d22d
dicacristian <cristian.dica28@gmail.com>
2023-05-02 16:57:59 +0200

    Tema ordonarea numerelor in mod descrescator

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://demoqa.com/sortable");
+        String xPathDescedentChilds = "//div[@class ='vertical-list-container mt-4']";
+        List<WebElement> descedentChilds = driver.findElements(By.xpath(xPathDescedentChilds));
+        Actions actions = new Actions(driver);
+        for(int i = descedentChilds.size() - 1 ; i>=0;i--){
+            actions.dragAndDrop(descedentChilds.get(i+1),descedentChilds.get(i)).perform();
+            driver.quit();
+
+
+    }
+    }
+}
+
